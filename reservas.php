adf73bc642bca812fe45e1726eb1a6e6e4a75b8a
unknown <9074.joan23@fje.edu>
2015-11-03 09:38:35 +0100

    login y reserva con sessions

--- reservas.php
+++ reservas.php
@@ -1,3 +1,45 @@
+<?php
+session_start();
+	if(isset($_POST['login']))$login=1;
+	if(isset($_POST['reservar']))$login=1;
+	if(isset($login)){
+		if(isset($_POST['login'])){
+		$_SESSION['nombre'] = $_POST['mail'];
+	
+		if(isset($_POST['mail']))$mail = $_POST['mail'];
+		if(isset($_POST['contraseña']))$contraseña = $_POST['contraseña'];
+		$con = mysqli_connect('localhost', 'root', 'DAW22015', 'bd_reservas');
+		$sql=("SELECT * FROM `tbl_usuario` WHERE usu_email = '$mail' && usu_contra = '$contraseña' ");
+		//echo $sql;
+		$datos = mysqli_query($con, $sql);
+		if(mysqli_num_rows($datos) > 0){
+			while($send = mysqli_fetch_array($datos)){
+				$send['usu_nom'] = utf8_encode($send['usu_nom']);
+				//echo "<br/><br/>$send[usu_nom]";
+				$_SESSION['nombre']=$send['usu_nom'];
+			}
+		}else{
+		$_SESSION['error'] = 'error';
+			header("Location: index.php");
+			die();
+		}
+		mysqli_close($con);
+		}
+		
+		if(isset($_POST['reservar'])){
+			
+			if(isset($_POST['reservar']))$reservar = $_POST['reservar'];
+			$con = mysqli_connect('localhost', 'root', 'DAW22015', 'bd_reservas');
+			//echo $reservar;
+			//$sql1=("SELECT * FROM `tbl_recursos` WHERE rec_id = $reservar");
+			//echo $sql1;
+			$sql=("UPDATE tbl_recursos SET rec_reservado = 0 WHERE rec_id = $reservar ");
+			//echo $sql;
+			$datos = mysqli_query($con, $sql);			
+			mysqli_close($con);
+			echo "Recurs reserva't";
+		}
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,7 +50,9 @@
     </head>
 
     <body>
-
+<?php
+echo "<br/>Bienvenido $_SESSION[nombre]";
+?>
     <div class="header"><h1>HEAD</h1></div>
     
     <div class="fondo">
@@ -18,7 +62,7 @@
             <h1>AULAS</h1>
  
             <?php
-            $con = mysqli_connect('localhost', 'root', '', 'bd_reservas');
+            $con = mysqli_connect('localhost', 'root', 'DAW22015', 'bd_reservas');
             $sql = ("SELECT * FROM `tbl_recursos` WHERE tbl_recursos.rec_tipo_rec= 1");
 
                 $datos = mysqli_query($con, $sql);
@@ -27,7 +71,20 @@
                             $cerca['rec_contingut']= utf8_encode($cerca['rec_contingut']);
 
                             echo "<div class='objeto'><div class='objeto2'>$cerca[rec_contingut]<br/></div>";
-                            echo "<input type='submit' value='RESERVAR'/></div>";
+							echo "<form action='#' method='POST'>";
+							echo "<input type='hidden' name='reservar' value='$cerca[rec_id]'/>";
+							if($cerca['rec_reservado']=="1"){
+								echo "<input type='submit' value='RESERVAR'/>";
+							}else{
+								echo "<input type='submit' value='RETORNAR'/>";
+							}
+							if($_SESSION['nombre']=='IT'){
+								echo "  <input type='hidden' name='manteniment' value='$cerca[rec_id]'/>";
+								echo "  <input type='submit' value='MANTENIMENT'/><br/>";
+							}
+							
+							echo "</form>";
+							echo "</div>";
                         }
                     }
             mysqli_close($con);
@@ -37,7 +94,7 @@
         <div class="aside"><h1>MATERIALES</h1>
             
             <?php
-            $con = mysqli_connect('localhost', 'root', '', 'bd_reservas');
+            $con = mysqli_connect('localhost', 'root', 'DAW22015', 'bd_reservas');
             $sql = ("SELECT * FROM `tbl_recursos` WHERE tbl_recursos.rec_tipo_rec= 0");
 
                 $datos = mysqli_query($con, $sql);
@@ -45,8 +102,21 @@
                         while($cerca = mysqli_fetch_array($datos)){
                             $cerca['rec_contingut']= utf8_encode($cerca['rec_contingut']);
 
-                            echo "<div class='objeto'><div class='objeto2'>$cerca[rec_contingut]<br/></div>";
-                            echo "<input type='submit' value='RESERVAR'/></div>";
+                              echo "<div class='objeto'><div class='objeto2'>$cerca[rec_contingut]<br/></div>";
+							echo "<form action='#' method='POST'>";
+							echo "<input type='hidden' name='reservar' value='$cerca[rec_id]'/>";
+							if($cerca['rec_reservado']=="1"){
+								echo "<input type='submit' value='RESERVAR'/>";
+							}else{
+								echo "<input type='submit' value='RETORNAR'/>";
+							}
+							if($_SESSION['nombre']=='IT'){
+								echo "  <input type='hidden' name='manteniment' value='$cerca[rec_id]'/>";
+								echo "  <input type='submit' value='MANTENIMENT'/><br/>";
+							}
+							
+							echo "</form>";
+							echo "</div>";
                         }
                     }
             mysqli_close($con);
@@ -56,3 +126,11 @@
     </div>
     </body>
 </html>
+<?php
+}else{
+	$_SESSION['validarse'] = 'error';
+	header("Location: index.php");
+	die();
+}
+
+?>
